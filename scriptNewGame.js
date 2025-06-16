09d3319451a0b0f1ce68e7591799c7f1e61d0987
destrier2 <zoe.li@student.tdsb.on.ca>
2025-06-16 11:36:18 -0400

    Update scriptNewGame.js

--- scriptNewGame.js
+++ scriptNewGame.js
@@ -2,20 +2,6 @@ const PLAYERS_KEY = "players";
 const TEAMS_KEY = "teams";
 let teamName;
 
-document.getElementById("other").addEventListener ("click", function(event) {
-    document.getElementById("otherInput").style.display="inline";
-});
-
-document.getElementById("fours").addEventListener("click", function(event) {
-    document.getElementById("otherInput").style.display="none";
-});
-document.getElementById("fives").addEventListener("click", function(event) {
-    document.getElementById("otherInput").style.display="none";
-});
-document.getElementById("sevens").addEventListener("click", function(event) {
-    document.getElementById("otherInput").style.display="none";
-});
-
 document.getElementById("newGameForm").addEventListener("submit", function(event) {
     let tname = document.getElementById("newGameTeamName").value;
     if (!validGame(tname)){
@@ -37,3 +23,26 @@ function validGame(teamname) {
     const exists = teams.some(team => team.name === teamname);
     return exists;
 }
+
+//Ensure that if the user says Other, then the number of players box pops up
+document.getElementById("other").addEventListener ("click", function(event) {
+    document.getElementById("otherInput").style.display="inline";
+});
+
+document.getElementById("fours").addEventListener("click", function(event) {
+    document.getElementById("otherInput").style.display="none";
+});
+document.getElementById("fives").addEventListener("click", function(event) {
+    document.getElementById("otherInput").style.display="none";
+});
+document.getElementById("sevens").addEventListener("click", function(event) {
+    document.getElementById("otherInput").style.display="none";
+});
+
+//If the user says there is a half, the length can be set
+document.getElementById("yesHalf").addEventListener("click", function(event) {
+    document.getElementById("hiddenHalfStuff").style.display="inline";
+});
+document.getElementById("noHalf").addEventListener("click", function(event) {
+    document.getElementById("hiddenHalfStuff").style.display="none";
+});
