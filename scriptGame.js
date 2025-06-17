8b68df1b8e71219560365a8e8388bf9ee5fbc9f7
destrier2 <zoe.li@student.tdsb.on.ca>
2025-06-17 11:37:36 -0400

    Update scriptGame.js

--- scriptGame.js
+++ scriptGame.js
@@ -4,13 +4,18 @@ let offense;
 let points;
 let pointsOpponent;
 let timeouts;
+let current; //Person currently with the disc
 
 function startTimer() {
     let seconds = sessionStorage.getItem("gameLength") *60;
     const intervalID = setInterval(function() {
         seconds--;
         let minutes = Math.floor(seconds/60);
-        document.getElementById("timeHere").innerHTML=minutes+":"+seconds%60;
+        if (seconds%60 < 10) {
+            document.getElementById("timeHere").innerHTML=minutes+":0"+seconds%60;
+        } else {
+            document.getElementById("timeHere").innerHTML=minutes+":"+seconds%60;
+        }
         document.getElementById("timeoutsHere").innerHTML="Timeouts remaining: "+timeouts;
         document.getElementById("pointsHere").innerHTML=points+" : "+pointsOpponent;
     }, 1000);
@@ -30,14 +35,6 @@ function loadButtons() {
     points = 0;
     pointsOpponent = 0;
     offense = sessionStorage.getItem("startOffense");
-    //Load players (do this for whenever the buttons are clicked)
-    /*let players = [];
-    try {
-		players = JSON.parse(localStorage.getItem(PLAYERS_KEY)) || [];
-	} catch (e) {
-		alert("Error. Please try again.");
-		return false; //exit if there's an issue
-	}*/
 	//Load the teams
 	let teams = [];
 	try {
@@ -51,18 +48,22 @@ function loadButtons() {
         const button = document.createElement("button");
         button.textContent = player;
         button.style.width="100%";
+        button.addEventListener("click", function(event) {
+            let players = [];
+            try {
+                players = JSON.parse(localStorage.getItem(PLAYERS_KEY)) || [];
+            } catch (e) {
+                alert("Error. Please try again.");
+                return false; //exit if there's an issue
+            }
+            alert(button.innerHTML+"clicked"); 
+            current = button.innerHTML;
+            //Add player to the history of people with the disc.
+            //Set the current player to this person
+        });
         let div = document.getElementById("playerButtons");
         div.appendChild(button);
         const newline = document.createElement("br");
         div.appendChild(newline);
     });
-    /*	//Check if the player exists
-	const exists = players.some(player => player.name === playername);
-	if (exists) { //If player exists, add player to team
-		const team = teams.find(team => team.name === teamName);
-		team.players.push(playername);
-		localStorage.setItem(TEAMS_KEY, JSON.stringify(teams));
-	} else {
-		alert("No player by this name found");
-	}*/
 }
