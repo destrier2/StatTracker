38f4c07cf729bf32a0d2c26f76f2cf214d162f33
destrier2 <zoe.li@student.tdsb.on.ca>
2025-07-20 08:48:26 -0400

    Added serviceworker and icon
    Also connected the manifest to all the html files for each page

--- serviceworker.js
+++ serviceworker.js
@@ -5,7 +5,46 @@ const APP_STATIC_RESOURCES = [
 	"/",
 	"/index.html",
 	"/style.css",
-	"/app.js",
-	"/newGame.html",
-	"/newTeam.html",
+	"/StatTrackerIcon.png",
+	"game.html",
+	"manifest.json",
+	"newGame.html",
+	"newPlayer.html",
+	"newTeam.html",
+	"viewGames.html",
+	"viewPlayers.html",
+	"viewTeam.html",
+	"script.js",
+	"scriptNewGame.js",
+	"scriptNewPlayer.js",
+	"scriptNewTeam.js",
+	"scriptViewGames.js",
+	"scriptViewPlayers.js"
 ];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(APP_STATIC_RESOURCES);
+    })(),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const names = await caches.keys();
+      await Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+          return undefined;
+        }),
+      );
+      await clients.claim();
+    })(),
+  );
+});
+
