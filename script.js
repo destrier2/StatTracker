eb8b8324141d7ccbb7b8ce417ca9256fe77abdca
destrier2 <zoe.li@student.tdsb.on.ca>
2025-08-18 10:22:07 -0400

    Allow user to change colors

--- script.js
+++ script.js
@@ -30,7 +30,83 @@ window.onload = function() {
 	if (!navigator.onLine) {
 		document.getElementById("ifOffline").style.display="initial";
 	}
+	try {
+		let color = localStorage.getItem("color");
+		if (color.length == 7) {
+			setColors(color);
+		} else {
+			throw TypeError;
+		}
+	} catch (e) {
+		localStorage.removeItem("color"); //if it exists
+		document.getElementById("colorPicker").value="#f7cac9";
+	}
+	try {
+		let bgcolor = localStorage.getItem("bgColor");
+		const r = document.querySelector(':root');
+		r.style.setProperty("--background", bgcolor);
+		document.getElementById("bgColorPicker").value = bgcolor;
+	} catch (e) {
+		localStorage.removeItem("bgColor"); //Delete bg color if it exists
+	}
 };
+
+document.getElementById("bgColorPicker").addEventListener("input", function(event) {
+	let bgcolor = document.getElementById("bgColorPicker").value;
+	const r = document.querySelector(':root');
+	localStorage.setItem("bgColor", bgcolor);
+	r.style.setProperty("--background", bgcolor);
+})
+
+function setColors(color) {
+	const r = document.querySelector(':root');
+	r.style.setProperty("--main", color);
+	document.getElementById("colorPicker").value=color;
+	const red = parseInt(color.substring(1,3), 16);
+	const green = parseInt(color.substring(3,5), 16);
+	const blue = parseInt(color.substring(5,7), 16);
+
+	let redVal;
+	let greenVal;
+	let blueVal;
+	
+	if (red < 40 || blue < 40 || green < 40) {
+		redVal = Math.min(red+50, 255);
+		blueVal = Math.min(blue+50, 255);
+		greenVal = Math.min(green+50, 255);
+	} else {
+		redVal = Math.max(red-40, 0);
+		blueVal = Math.max(blue-40, 0);
+		greenVal = Math.max(green-40, 0);
+	}
+	let newRed = (redVal.toString(16));
+	let newGreen = (greenVal.toString(16));
+	let newBlue = (blueVal.toString(16));
+	//Set text to white or black
+	if ((red+blue+green)/3 < 255/2) {
+		r.style.setProperty("--text", "white");
+	} else {
+		r.style.setProperty("--text", "black");
+	}
+	//Add length if necessary
+	if (newRed.length < 2) {
+		newRed = "0"+newRed;
+	}
+	if (newBlue.length < 2) {
+		newBlue = "0"+newBlue;
+	}
+	if (newGreen.length <2) {
+		newGreen = "0"+newGreen;
+	}
+	let borderColor = "#"+newRed+newGreen+newBlue;
+	r.style.setProperty("--accent", borderColor);
+	document.getElementById("borderColor").value = borderColor;
+}
+
+document.getElementById("colorPicker").addEventListener("input", function(event) {
+	setColors(document.getElementById("colorPicker").value);
+	localStorage.setItem("color", document.getElementById("colorPicker").value);
+})
 
 /*document.getElementById("addPlayer").addEventListener("click", function(event) {
 	document.getElementById('hiddenAddPlayer').style.display='block';
--- scriptViewTeam.js
+++ scriptViewTeam.js
@@ -96,16 +96,50 @@ document.getElementById("teamName").addEventListener("keydown", function(event) 
 function teamEntered() {
 	teamName = document.getElementById('teamName').value;
 	openTeam();
-}
-
-window.onload = function() {
-	if (sessionStorage.getItem("teamName") !== null) {
-		teamName = sessionStorage.getItem("teamName");
-		openTeam();
-		console.log("team name should be ", teamName);
-
-	}
 };
+
+function setColors(color) {
+	const r = document.querySelector(':root');
+	r.style.setProperty("--main", color);
+	const red = parseInt(color.substring(1,3), 16);
+	const green = parseInt(color.substring(3,5), 16);
+	const blue = parseInt(color.substring(5,7), 16);
+
+	let redVal;
+	let greenVal;
+	let blueVal;
+	
+	if (red < 40 || blue < 40 || green < 40) {
+		redVal = Math.min(red+50, 255);
+		blueVal = Math.min(blue+50, 255);
+		greenVal = Math.min(green+50, 255);
+	} else {
+		redVal = Math.max(red-40, 0);
+		blueVal = Math.max(blue-40, 0);
+		greenVal = Math.max(green-40, 0);
+	}
+	let newRed = (redVal.toString(16));
+	let newGreen = (greenVal.toString(16));
+	let newBlue = (blueVal.toString(16));
+	//Set text to white or black
+	if ((red+blue+green)/3 < 255/2) {
+		r.style.setProperty("--text", "white");
+	} else {
+		r.style.setProperty("--text", "black");
+	}
+	//Add length if necessary
+	if (newRed.length < 2) {
+		newRed = "0"+newRed;
+	}
+	if (newBlue.length < 2) {
+		newBlue = "0"+newBlue;
+	}
+	if (newGreen.length <2) {
+		newGreen = "0"+newGreen;
+	}
+	let borderColor = "#"+newRed+newGreen+newBlue;
+	r.style.setProperty("--accent", borderColor);
+}
 
 window.onbeforeunload = function() {
 	if (teamName){
@@ -280,5 +314,27 @@ window.addEventListener("offline", (e) => {
 window.onload = function() {
 	if (!navigator.onLine) {
 		document.getElementById("ifOffline").style.display="initial";
+	}
+	try {
+		let color = localStorage.getItem("color");
+		if (color.length == 7) {
+			setColors(color);
+		} else {
+			throw TypeError;
+		}
+	} catch (e) {
+		localStorage.removeItem("color"); //if it exists
+	}
+	if (sessionStorage.getItem("teamName") !== null) {
+		teamName = sessionStorage.getItem("teamName");
+		openTeam();
+		console.log("team name should be ", teamName);
+	}
+	try {
+		let bgcolor = localStorage.getItem("bgColor");
+		const r = document.querySelector(':root');
+		r.style.setProperty("--background", bgcolor);
+	} catch (e) {
+		localStorage.removeItem("bgColor"); //Delete bg color if it exists
 	}
 };
